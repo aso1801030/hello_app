5cfbf08b4830e220226ab0de8e0320b5f1518f8b
aso1801030 <1801030@s.asojuku.ac.jp>
2020-06-30 05:49:11 +0000

    テスト

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <HelloWorld v-bind:title="massage" v-on:result-event='appAction' />
+    <P>{{result}}</P>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data: function(){
+    return{
+      massage:"HELLO",
+    };
+  },
+  methods:{
+    doAction:function(){
+      var input = prompt("new title:");
+      this.massage = input;
+    },
+    appAction:function(massage){
+      this.result = '(*** you send:"' + massage + '".***)'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <p>{{ massage }}</p>
+    <hr>
+    <div>
+      <input type="text" v-model="input">
+      <button v-on:click="doAction">Click</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'Helloworld',
+  props:{
+    title:String,
+  },
+  data:function(){
+    return{
+      massage:'お名前は？',
+      input:'no name',
+    };
+  },
+  methods:{
+    doAction:function(){
+      this.massage = 'こんにちは、' + this.input + 'さん';
+      this.$emit('result-event',this.input)
+    }
+  }
+}
+</script>
+
+<style>
+  div{
+    margin:0px;
+    padding:0px;
+    text-align:left;
+  }
+  h1{
+    font-size:72pt;
+    font-weight: bold;
+    text-align: ringht;
+    letter-spacing: -8px;
+    color:#f0f0f0;
+    margin: 0px;
+  }
+  p{
+    margin: 0px;
+    color:#666;
+    font-size:16pt;
+  }
+</style>
